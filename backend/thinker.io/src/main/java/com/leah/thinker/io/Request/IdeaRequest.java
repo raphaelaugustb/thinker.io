96eb8a8458a7ab73ae5b908a1f2afc580f5fb101
raphaelaugustb <travassosraphael12@gmail.com>
2024-06-29 16:20:42 -0300

    Implement finished boolean on requests

--- backend/thinker.io/src/main/java/com/leah/thinker/io/Request/IdeaRequest.java
+++ backend/thinker.io/src/main/java/com/leah/thinker/io/Request/IdeaRequest.java
@@ -1,6 +1,18 @@
 package com.leah.thinker.io.Request;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class IdeaRequest {
     private String tittle;
     private String description;
+    private boolean finished;
 }
