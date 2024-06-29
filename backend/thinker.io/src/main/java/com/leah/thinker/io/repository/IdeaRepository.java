6c6a821b2708c830b58e7a6bdff3a2ed928e7873
raphaelaugustb <travassosraphael12@gmail.com>
2024-06-29 16:20:09 -0300

    Implement finished or not on requests

--- backend/thinker.io/src/main/java/com/leah/thinker/io/repository/IdeaRepository.java
+++ backend/thinker.io/src/main/java/com/leah/thinker/io/repository/IdeaRepository.java
@@ -3,6 +3,11 @@ package com.leah.thinker.io.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.leah.thinker.io.entity.Idea;
+
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface IdeiaRepository extends JpaRepository<Idea,Long> {
+public interface IdeaRepository extends JpaRepository<Idea,Long> {
+    List<Idea> findByTitle(String title, UUID idUser);
 }
