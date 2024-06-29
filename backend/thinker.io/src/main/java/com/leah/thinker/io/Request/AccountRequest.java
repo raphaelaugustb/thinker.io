e6245c19826a7ce2ce7a3fc711ca66b3bbfc3a11
raphaelaugustb <travassosraphael12@gmail.com>
2024-06-29 14:32:36 -0300

    AccountRequest created

---
+++ backend/thinker.io/src/main/java/com/leah/thinker/io/Request/AccountRequest.java
@@ -0,0 +1,16 @@
+package com.leah.thinker.io.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class AccountRequest {
+    private String emaill;
+    private String username;
+    private String password;
+}
