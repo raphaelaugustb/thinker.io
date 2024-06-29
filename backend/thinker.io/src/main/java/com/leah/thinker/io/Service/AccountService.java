5c17ae11ef1f9e8daeec54f0afc98c8c89e80df3
raphaelaugustb <travassosraphael12@gmail.com>
2024-06-29 14:32:53 -0300

    Implementation CRUD finished

--- backend/thinker.io/src/main/java/com/leah/thinker/io/Service/AccountService.java
+++ backend/thinker.io/src/main/java/com/leah/thinker/io/Service/AccountService.java
@@ -1,4 +1,50 @@
 package com.leah.thinker.io.Service;
 
+import com.leah.thinker.io.Request.AccountRequest;
+import com.leah.thinker.io.entity.Account;
+import com.leah.thinker.io.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.management.openmbean.InvalidOpenTypeException;
+import java.io.InvalidObjectException;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class AccountService {
+    @Autowired
+    AccountRepository accountRepository;
+    public void createNewAccount(Account account) {
+        if (account == null){
+            throw  new NullPointerException("Conta inválida");
+        } else {
+            accountRepository.save(account);
+        }
+    }
+    public Account getAccountInfoById(UUID id) {
+        if (accountRepository.existsById(id)) {
+            return accountRepository.findById(id).get();
+        } else {
+            throw new RuntimeException("Usuário não cadastrado");
+        }
+    }
+    public void deleteAccountById(UUID id) {
+        if (accountRepository.existsById(id)) {
+            accountRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Conta não encontrada");
+        }
+    }
+    public void updateAccount(UUID id, AccountRequest accountRequest) {
+        if (accountRepository.existsById(id)) {
+            Account account = accountRepository.findById(id).get();
+            account.setEmail(accountRequest.getEmaill());
+            account.setUsername(accountRequest.getUsername());
+            account.setPassword(account.getPassword());
+        } else {
+            throw new RuntimeException("Conta não encontrada");
+        }
+
+    }
 }
